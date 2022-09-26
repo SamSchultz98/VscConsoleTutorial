1d7ebf7f176e6f5bd7968734de2b70b8e68b4fc3
Sam Schultz <sam.schultz@outlook.com>
2022-09-26 11:29:59 -0400

    Working on Transfer

--- Models/Account.cs
+++ Models/Account.cs
@@ -46,25 +46,34 @@ namespace Banking.Models;
 
         }
 
-         public void Transfer(int accountNo, int amount)          //Asking for the account no and what they want to transgfer
+         public void Transfer(int accountNo,int ogaccount, int amount)          //Asking for the account no and what they want to transgfer
         {
         List<Account> accounts = new();
+        var takeaccountl =from t in accounts               //Finding the account to take from
+                        where t.AccountNo == ogaccount
+                        select t;
 
         var targaccounts = from t in accounts               //Finding the account they are looking for
                         where t.AccountNo == accountNo
                         select t;
 
         var targetaccount = targaccounts.FirstOrDefault();  //Pulling that account out of the list (should be only one)
-
-        if (targetaccount is null)  //If it was unable to find a account, it will come back null
+        var takeaccount = takeaccountl.FirstOrDefault();
+        if (takeaccount is null)
         {
-        System.Console.WriteLine("No account matching that account number");
-        }
-        targetaccount.Balance =- amount; //Adding the amount to the target account's balance
+            System.Console.WriteLine("The account you are trying to send money from was not found");
 
-        System.Console.WriteLine($"The new balance is {targetaccount.Balance}");    //Returning the account balance
 
         }
+        if (targetaccount is null)
+        {
+            System.Console.WriteLine("The account you were attempting to send money to did not exisit");
+        }
+
+        
+
+        }
+        
 
 
     }
