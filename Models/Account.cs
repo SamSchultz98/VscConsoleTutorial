c99b15d7824804233e42538e61717454fff7536c
Sam Schultz <sam.schultz@outlook.com>
2022-09-26 11:25:21 -0400

    Init2

---
+++ Models/Account.cs
@@ -0,0 +1,71 @@
+namespace Banking.Models;
+    public class Account
+    {
+    public int AccountNo {get; set;}
+    public string Description { get; set; } = string.Empty;
+    public decimal Balance { get; set; }
+
+    public void Deposit(int accountNo, int amount)          //Asking for the account no and the amount they want to deposit
+        {
+        List<Account> accounts = new();
+
+        var targaccounts = from t in accounts               //Finding the account they are looking for
+                        where t.AccountNo == accountNo
+                        select t;
+
+        var targetaccount = targaccounts.FirstOrDefault();  //Pulling that account out of the list (should be only one)
+
+        if (targetaccount is null)  //If it was unable to find a account, it will come back null
+        {
+        System.Console.WriteLine("No account matching that account number");
+        }
+        targetaccount.Balance =+ amount; //Adding the amount to the target account's balance
+
+        System.Console.WriteLine($"The new balance is {targetaccount.Balance}");    //Returning the account balance
+
+        }
+
+
+        public void Withdraw(int accountNo, int amount)          //Asking for the account no and the amount they want to deposit
+        {
+        List<Account> accounts = new();
+
+        var targaccounts = from t in accounts               //Finding the account they are looking for
+                        where t.AccountNo == accountNo
+                        select t;
+
+        var targetaccount = targaccounts.FirstOrDefault();  //Pulling that account out of the list (should be only one)
+
+        if (targetaccount is null)  //If it was unable to find a account, it will come back null
+        {
+        System.Console.WriteLine("No account matching that account number");
+        }
+        targetaccount.Balance =- amount; //Removing the amount to the target account's balance
+
+        System.Console.WriteLine($"The new balance is {targetaccount.Balance}");    //Returning the account balance
+
+        }
+
+         public void Transfer(int accountNo, int amount)          //Asking for the account no and what they want to transgfer
+        {
+        List<Account> accounts = new();
+
+        var targaccounts = from t in accounts               //Finding the account they are looking for
+                        where t.AccountNo == accountNo
+                        select t;
+
+        var targetaccount = targaccounts.FirstOrDefault();  //Pulling that account out of the list (should be only one)
+
+        if (targetaccount is null)  //If it was unable to find a account, it will come back null
+        {
+        System.Console.WriteLine("No account matching that account number");
+        }
+        targetaccount.Balance =- amount; //Adding the amount to the target account's balance
+
+        System.Console.WriteLine($"The new balance is {targetaccount.Balance}");    //Returning the account balance
+
+        }
+
+
+    }
+    
---
+++ Program.cs
@@ -0,0 +1,8 @@
+﻿Console.WriteLine("Hello, World!");
+
+
+int I = 0;
+
+I += 5;
+
+System.Console.WriteLine(I);
